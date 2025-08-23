f6e4e4bd89954f7fbdc55b38b755779aec81de5e
Tan Minh Nguyen <nguyenminhtan250406@gmail.com>
2025-08-23 14:50:56 -0700

    add solution for ll-prepend

---
+++ LLCodingExercises/LLPrepend/Main.java
@@ -0,0 +1,60 @@
+
+package LLCodingExercises.LLPrepend;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(2);
+        myLinkedList.append(3);
+
+        System.out.println("Before prepend():");
+        System.out.println("-----------------");
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        System.out.println("\nLinked List:");
+        myLinkedList.printList();
+
+        myLinkedList.prepend(1);
+
+        System.out.println("\n\nAfter prepend():");
+        System.out.println("----------------");
+        myLinkedList.getHead();
+        myLinkedList.getTail();
+        myLinkedList.getLength();
+
+        System.out.println("\nLinked List:");
+        myLinkedList.printList();
+        
+        /*
+            EXPECTED OUTPUT:
+    
+            Before prepend():
+            -----------------
+            Head: 2
+            Tail: 3
+            Length: 2
+    
+            Linked List:
+            2
+            3
+    
+    
+            After prepend():
+            ----------------
+            Head: 1
+            Tail: 3
+            Length: 3
+    
+            Linked List:
+            1
+            2
+            3
+
+        */
+        
+    }
+    
+}
