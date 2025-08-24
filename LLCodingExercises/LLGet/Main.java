1a673ff94bee62942727b063a7ad649b2af0c856
Tan Minh Nguyen <nguyenminhtan250406@gmail.com>
2025-08-24 10:02:36 -0700

    add solution for ll-get

---
+++ LLCodingExercises/LLGet/Main.java
@@ -0,0 +1,26 @@
+
+package LLCodingExercises.LLGet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        LinkedList myLinkedList = new LinkedList(0);
+        myLinkedList.append(1);
+        myLinkedList.append(2);
+        myLinkedList.append(3);
+
+
+        System.out.println(myLinkedList.get(3).value);
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            3
+
+        */
+
+    }
+
+}
