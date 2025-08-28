e16f5f1c257d3531e2b5b4b6f04320d8fe8b890c
Tan Minh Nguyen <nguyenminhtan250406@gmail.com>
2025-08-27 21:36:26 -0700

    add solution for dll-constructor

---
+++ DLLCodingExcercises/DLLConstructor/Main.java
@@ -0,0 +1,27 @@
+
+package DLLCodingExcercises.DLLConstructor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(7);
+        
+        myDLL.printAll();
+        
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Head: 7
+            Tail: 7
+            Length: 1
+            
+            Linked List:
+            7
+
+        */
+
+    }
+
+}
