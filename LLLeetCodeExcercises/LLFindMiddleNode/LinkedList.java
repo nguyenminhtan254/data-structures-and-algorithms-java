0c1700f7c3b85468e44519a9c0968ef56cb839fa
Tan Minh Nguyen <nguyenminhtan250406@gmail.com>
2025-08-27 20:32:57 -0700

    add solution for ll-find-middle-node

---
+++ LLLeetCodeExcercises/LLFindMiddleNode/LinkedList.java
@@ -0,0 +1,86 @@
+
+package LLLeetCodeExcercises.LLFindMiddleNode;
+
+public class LinkedList {
+
+    private Node head;
+    private Node tail;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public Node getTail() {
+        return tail;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void printAll() {
+        if (head == null) {
+            System.out.println("Head: null");
+            System.out.println("Tail: null");
+        } else {
+            System.out.println("Head: " + head.value);
+            System.out.println("Tail: " + tail.value);
+        }
+        System.out.println("\nLinked List:");
+        if (head == null) {
+            System.out.println("empty");
+        } else {
+            printList();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        tail = null;
+    }
+
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+
+    public Node findMiddleNode() {
+        if (head == null) return null;
+        if (head.next == null) return head;
+
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast != tail) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        
+        return slow;
+    }
+
+}
