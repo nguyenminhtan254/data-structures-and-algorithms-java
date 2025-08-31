6d55a52e8081b94d818c308cdb46a0d82361bf6c
Tan Minh Nguyen <nguyenminhtan250406@gmail.com>
2025-08-31 16:17:58 -0700

    add solution for dll-get

---
+++ DLLCodingExcercises/DLLGet/Main.java
@@ -0,0 +1,26 @@
+
+package DLLCodingExcercises.DLLGet;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        DoublyLinkedList myDLL = new DoublyLinkedList(0);
+        myDLL.append(1);
+        myDLL.append(2);
+        myDLL.append(3);
+
+
+        System.out.println(myDLL.get(4).value);
+
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            3
+
+        */
+
+    }
+
+}
