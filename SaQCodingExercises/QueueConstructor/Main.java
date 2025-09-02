00e379ac46f81bf46fc95efec75f3d54b8d0e1ca
Tan Minh Nguyen <nguyenminhtan250406@gmail.com>
2025-09-02 13:31:03 -0700

    add solution for queue-constructor

---
+++ SaQCodingExercises/QueueConstructor/Main.java
@@ -0,0 +1,26 @@
+
+package SaQCodingExercises.QueueConstructor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Queue myQueue = new Queue(4);
+
+        myQueue.printAll();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            First: 4
+            Last: 4
+            Length: 1
+            
+            Queue:
+            4
+
+        */
+
+    }
+
+}
