6b24b192a5dda3ce2b3f2f7d9778405094339f48
Tan Minh Nguyen <nguyenminhtan250406@gmail.com>
2025-09-03 19:19:12 -0700

    add solution for bst-constructor

---
+++ BSTCodingExercises/BSTConstructor/BinarySearchTree.java
@@ -0,0 +1,22 @@
+
+package BSTCodingExercises.BSTConstructor;
+
+public class BinarySearchTree {
+
+    private Node root;
+
+    class Node {
+        int value;
+        Node left;
+        Node right;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+	
+	public Node getRoot() {
+        return root;
+    }
+
+}
