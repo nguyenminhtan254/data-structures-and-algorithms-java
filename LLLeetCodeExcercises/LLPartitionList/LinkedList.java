0fce40facd8b8d275d1fe63bb946f0808b7634d6
Tan Minh Nguyen <nguyenminhtan250406@gmail.com>
2025-09-12 15:09:48 -0700

    add solution for ll-partition-list

---
+++ LLLeetCodeExcercises/LLPartitionList/LinkedList.java
@@ -0,0 +1,95 @@
+
+package LLLeetCodeExcercises.LLPartitionList;
+
+public class LinkedList {
+
+    private Node head;
+    private int length;
+
+    class Node {
+        int value;
+        Node next;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        length = 1;
+    }
+
+    public Node getHead() {
+        return head;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public void printList() {
+        Node temp = head;
+        if (temp == null) {
+            System.out.println("empty");
+        } else {
+            while (temp != null) {
+                System.out.print(temp.value);
+                temp = temp.next;
+                if (temp != null) {
+                    System.out.print(" -> ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    
+    public void makeEmpty() {
+        head = null;
+        length = 0;
+    }
+    
+    public void append(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+        length++;
+    }
+
+    public void partitionList(int x) {
+        if (head == null) return;
+
+        Node dummy1 = new Node(0);
+        Node dummy2 = new Node(0);
+        Node prev1 = dummy1;
+        Node prev2 = dummy2;
+
+        Node current = head;
+
+        while (current != null) {
+            if (current.value < x) {
+                prev1.next = current;
+                prev1 = prev1.next;
+            } else {
+                prev2.next = current;
+                prev2 = prev2.next;
+            }
+            current = current.next;
+        }
+
+        prev2.next = null;
+        prev1.next = dummy2.next;
+
+        head = dummy1.next;
+
+    }  
+
+}
