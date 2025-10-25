1f62f8d3bc052895f20767ee443c0e5c289b7fc5
Tan Minh Nguyen <nguyenminhtan250406@gmail.com>
2025-10-25 11:22:09 -0700

    add solution for graph-add-vertex

---
+++ GraphCodingExercises/GraphAddVertex/Graph.java
@@ -0,0 +1,27 @@
+
+package GraphCodingExercises.GraphAddVertex;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Graph {
+    
+    private HashMap<String, ArrayList<String>> adjList = new HashMap<>();
+
+    public HashMap<String, ArrayList<String>> getAdjList() {
+        return adjList;
+    }
+
+    public void printGraph() {
+        System.out.println(adjList);
+    }
+
+    public boolean addVertex(String vertex) {
+        if (adjList.get(vertex) == null) {
+            adjList.put(vertex, new ArrayList<String>());
+            return true;
+        }
+        return false;
+    }
+
+}
---
+++ GraphCodingExercises/GraphAddVertex/Main.java
@@ -0,0 +1,26 @@
+
+package GraphCodingExercises.GraphAddVertex;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Graph myGraph = new Graph();
+        
+        myGraph.addVertex("A");
+        myGraph.addVertex("B");
+        
+        System.out.println("Graph:");
+        myGraph.printGraph();
+
+        /*
+            EXPECTED OUTPUT:
+            ----------------
+            Graph:
+            {A=[], B=[]}
+
+        */
+
+    }
+
+}
